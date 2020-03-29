9a87cd8c0559fe709ee501f609bd5c8eac91bf77
Deep Toaster <deeptoaster@gmail.com>
2020-03-28 21:42:21 -0400

    made formatting consistent

---
+++ bin/js/closure.js
@@ -0,0 +1,223 @@
+function Node(x, y) {
+  this.x = x;
+  this.y = y;
+  this.theta = 0;
+  this.width = 0;
+}
+
+function Closure(presenter) {
+  var self = this;
+  var friction;
+  var grasp;
+  var nodes;
+  var g0;
+  var g1;
+
+  function lerp(n0, n1, t) {
+    return {
+      x: n0.x * (1 - t) + n1.x * t,
+      y: n0.y * (1 - t) + n1.y * t
+    };
+  }
+
+  function updateEdge(n0, n1, side) {
+		var a = n1.x - n0.x;
+		var b = n0.y - n1.y;
+
+		n0.theta = -Math.atan(b / a);
+		n0.width = Math.sqrt(a * a + b * b);
+
+		if (a < 0) {
+			n0.theta -= Math.PI;
+		}
+
+    presenter.updateEdge(n0, side);
+  }
+
+  function updateEdges(i) {
+    i = (i + nodes.length) % nodes.length;
+
+    if (nodes.length) {
+      updateEdge(
+				nodes[(i + nodes.length - 1) % nodes.length],
+        nodes[i]
+      );
+
+      updateEdge(nodes[i], nodes[(i + 1) % nodes.length]);
+    }
+  }
+
+  function updateGrasp() {
+    presenter.toggleGrasp(false);
+
+    if (grasp.length == 2) {
+      var t = grasp[0].theta - grasp[1].theta - Math.PI;
+
+      if (t < -Math.PI) {
+        t += 2 * Math.PI;
+      }
+
+      if (t < 0) {
+        t = -t;
+        grasp.reverse();
+      }
+
+      if (t >= friction * 2 || !grasp[0].width || !grasp[1].width) {
+        presenter.toggleGrasp(-1);
+        return;
+      }
+
+      var n0 = nodes[(nodes.indexOf(grasp[0]) + 1) % nodes.length];
+      var n1 = nodes[(nodes.indexOf(grasp[1]) + 1) % nodes.length];
+      var f0 = grasp[0].theta - Math.PI / 2 - friction;
+      var d0 = Math.sin(f0 - grasp[1].theta + Math.PI);
+
+      var b0 = (
+				(n1.y - grasp[0].y) * Math.cos(f0) - (n1.x - grasp[0].x) * Math.sin(f0)
+			) / d0;
+
+      var f1 = grasp[1].theta - Math.PI / 2 + friction;
+      var d1 = Math.sin(f1 - grasp[0].theta);
+
+      var b1 = (
+				(grasp[0].y - n1.y) * Math.cos(f1) - (grasp[0].x - n1.x) * Math.sin(f1)
+			) / d1;
+
+      var m = Math.sin(f0 - grasp[0].theta) / d0;
+
+      var p0 = grasp[1].width - (
+				(n0.x - n1.x) * (grasp[1].x - n1.x) + (n0.y - n1.y) *
+						(grasp[1].y - n1.y)
+			) / grasp[1].width;
+
+      var p1 = grasp[0].width - (
+				(grasp[1].x - grasp[0].x) * (n0.x - grasp[0].x) +
+						(grasp[1].y - grasp[0].y) * (n0.y - grasp[0].y)
+			) / grasp[0].width;
+
+      var s0 = p0 < p1
+				? Math.min(
+						(Math.max(m * grasp[1].width + b1, 0) + grasp[1].width - b0) /
+								(m + 1),
+						grasp[0].width
+					)
+				: grasp[0].width;
+
+      var s1 = p1 < p0
+				? Math.min(
+						(Math.max(m * grasp[0].width + b0, 0) + grasp[0].width - b1) /
+								(m + 1),
+						grasp[1].width
+					)
+				: grasp[1].width;
+
+      var t0 = Math.max((m * s1 + b1), 0);
+      var t1 = Math.max((m * s0 + b0), 0);
+      var h0 = lerp(grasp[0], n0, t0 / grasp[0].width);
+      var h1 = lerp(n1, grasp[1], s1 / grasp[1].width);
+
+      g0.x = h0.x;
+      g0.y = h0.y;
+      g1.x = h1.x;
+      g1.y = h1.y;
+      presenter.updateNode(g0);
+      presenter.updateNode(g1);
+      updateEdge(g0, lerp(grasp[0], n0, s0 / grasp[0].width), 'left');
+      updateEdge(g1, lerp(n1, grasp[1], t1 / grasp[1].width), 'right');
+      presenter.toggleGrasp(1);
+
+      presenter.updateGraph(
+				m,
+				b0,
+				b1,
+				grasp[0].width,
+				grasp[1].width,
+        s0,
+        t0,
+        s1,
+        t1
+       );
+    }
+  }
+
+  this.addGrasp = function(node) {
+    grasp.push(node);
+    updateGrasp();
+  }
+
+  this.removeGrasp = function(node) {
+    grasp.splice(grasp.indexOf(node), 1);
+    updateGrasp();
+  }
+
+  this.appendNode = function(x, y, grasp) {
+    if (typeof x == 'undefined') {
+      x = nodes.length ? nodes[nodes.length - 1].x : 0;
+    }
+
+    if (typeof y == 'undefined') {
+      y = nodes.length ? nodes[nodes.length - 1].y : 0;
+    }
+
+    var node = new Node(x, y);
+
+    presenter.createNode(node, nodes.length, grasp);
+
+    if (nodes.length && !grasp) {
+      updateEdge(nodes[nodes.length - 1], node);
+      updateEdge(node, nodes[0]);
+    }
+
+    if (!grasp) {
+      nodes.push(node);
+    }
+
+    return node;
+  };
+
+  this.moveNode = function(node, x, y) {
+    node.x = x;
+    node.y = y;
+    presenter.updateNode(node);
+    presenter.center(false);
+    updateEdges(nodes.indexOf(node));
+    updateGrasp();
+  }
+
+  this.removeNode = function(node) {
+    var i = nodes.indexOf(node);
+    var j = grasp.indexOf(node);
+
+    if (j >= 0) {
+      self.removeGrasp(node);
+    }
+
+    presenter.destroyNode(node);
+    nodes.splice(i, 1);
+    updateEdges(i - 1);
+    updateGrasp();
+  }
+
+  this.getMu = function() {
+    return friction * 180 / Math.PI;
+  }
+
+  this.setMu = function(mu) {
+    friction = mu * Math.PI / 180;
+    updateGrasp();
+  }
+
+  this.init = function() {
+    grasp = [];
+    nodes = [];
+    presenter.model = self;
+    presenter.init();
+    g0 = self.appendNode(0, 0, true);
+    g1 = self.appendNode(0, 0, true);
+    self.appendNode(-1, 1);
+    self.appendNode(1, 1);
+    self.appendNode(1, -1);
+    self.appendNode(-1, -1);
+    presenter.center(true);
+  };
+}
---
+++ bin/js/closurepresenter.js
@@ -0,0 +1,252 @@
+function ClosurePresenter() {
+  var padding = 0.6;
+  var mu = 30;
+  var self = this;
+  var top;
+  var right;
+  var bottom;
+  var left;
+  var $grid;
+
+  this.center = function(init) {
+    var h = $('#game').height();
+    var w = $('#game').width();
+    var fs = 1 / Math.max((top - bottom) / (h * padding), (right - left) /
+        (w * padding));
+
+    var css = {
+      top: h / 2 + (top + bottom) * fs / 2 + 'px',
+      left: w / 2 - (right + left) * fs / 2 + 'px'
+    };
+
+    $('#game').css({
+      fontSize: fs,
+      backgroundPosition: css.left + ' ' + css.top
+    });
+
+    $grid.css(css);
+
+    if (init) {
+      setTimeout(function() {
+        $('#game').addClass('active');
+      }, 400);
+    }
+  };
+
+  this.updateEdge = function(n0, side) {
+		n0.$node.children().css({
+      width: n0.width + 'em',
+      transform: 'rotate(' + -n0.theta + 'rad)'
+    }).children().css('transform', function() {
+      var t = 90 + ($(this).hasClass('in')
+				? -self.model.getMu()
+				: self.model.getMu());
+
+      return 'rotate(' + ($(this).hasClass('left') ? t : -t) + 'deg)';
+    });
+
+    if (side) {
+      n0.$node.addClass(side);
+    }
+  };
+
+  this.toggleGrasp = function(mode) {
+    var $e = $('.node.grasp .edge, #output').removeClass('active error');
+
+    if (mode == -1) {
+      $e.addClass('error');
+    } else if (mode) {
+      $e.addClass('active');
+    }
+  }
+
+  this.updateGraph = function(m, b0, b1, w0, w1, s0, t0, s1, t1) {
+    var x0 = 0;
+    var y0 = 0;
+    var x1 = 0;
+    var y1 = 0;
+    var w = $('.graph').width();
+    var h = $('.graph').height();
+    var max = Math.max(w0, w1)
+
+    if (b0 < 0) {
+      x0 = -b0 / m * w / max;
+    } else {
+      y0 = b0 * h / max;
+    }
+
+    if (b1 < 0) {
+      y1 = -b1 / m * w / max;
+    } else {
+      x1 = b1 * h / max;
+    }
+
+    var t = Math.atan(m);
+    var wp = w0 * w / max;
+    var hp = w1 * h / max;
+    var d0 = Math.min((wp - x0) / Math.cos(t), (hp - y0) / Math.sin(t));
+    var d1 = Math.min((wp - x1) / Math.sin(t), (hp - y1) / Math.cos(t));
+
+    $('.graph .edge, .graph .box').remove();
+
+    var e = $('.graph').append(
+			'<span class="edge" /><span class="edge" /><span class="box" />'
+    ).children('.edge');
+
+    e.first().css({
+      top: h - y0,
+      left: x0,
+      width: d0,
+      transform: 'rotate(' + -t + 'rad)'
+    });
+
+    e.last().css({
+      top: h - y1,
+      left: x1,
+      width: d1,
+      transform: 'rotate(' + (t - Math.PI / 2) + 'rad)'
+    });
+
+    $('.graph .box').css({
+      top: h - s1 * h / max - 1,
+      right: w - s0 * w / max - 1,
+      bottom: t1 * h / max - 1,
+      left: t0 * w / max - 1,
+    });
+
+    $('.graph .axis').text(Math.round(max * 10) / 10);
+    $('#r0 .m').text(Math.round(m * 100) / 100);
+
+    if (Math.round(b0 * 100) / 100) {
+      $('#r0 .b').text(Math.abs(Math.round(b0 * 100) / 100));
+      $('#r0 .op').html(b0 < 0 ? '&minus;' : '+');
+    } else {
+      $('#r0 .b, #r0 .op').empty();
+    }
+
+    $('#r1 .m').text(Math.round(m * 100) / 100);
+
+    if (Math.round(b1 * 100) / 100) {
+      $('#r1 .b').text(Math.abs(Math.round(b1 * 100) / 100));
+      $('#r1 .op').html(b1 < 0 ? '&minus;' : '+');
+    } else {
+      $('#r1 .b, #r1 .op').empty();
+    }
+
+    $('#i0 .s').text(Math.round(t0 * 100) / 100);
+    $('#i0 .t').text(Math.round(s0 * 100) / 100);
+    $('#i1 .s').text(Math.round(t1 * 100) / 100);
+    $('#i1 .t').text(Math.round(s1 * 100) / 100);
+  }
+
+  this.createNode = function(node, i, grasp) {
+    node.$node = $('<span class="node"><span class="edge" /></span>');
+
+    if (grasp) {
+      node.$node.addClass('grasp').children().append(
+				'<span class="left in" /><span class="left out" />' +
+						'<span class="right in" /><span class="right out" />'
+			);
+    }
+
+    this.updateNode(node);
+    node.$node.appendTo($grid)
+    node.$node[0].node = node;
+
+    if (!grasp) {
+      node.$input = $(
+				'<li class="vertex"><button class="remove">&times;</button> (' +
+						'<input type="text" class="x" value="' + node.x	+
+						'" />, <input type="text" class="y" value="' + node.y +	'" />)</li>'
+			);
+
+      node.$input[0].node = node;
+
+      if (i) {
+        node.$input.insertAfter('#nodes > :nth-child(' + i + ')');
+      } else {
+        node.$input.prependTo('#nodes');
+      }
+    }
+  };
+
+  this.destroyNode = function(node) {
+    node.$input.remove();
+    node.$node.remove();
+  };
+
+  this.updateNode = function(node) {
+    top = Math.max(top, node.y);
+    right = Math.max(right, node.x);
+    bottom = Math.min(bottom, node.y);
+    left = Math.min(left, node.x);
+
+    node.$node.css({
+      top: -node.y + 'em',
+      left: node.x + 'em'
+    });
+  };
+
+  this.init = function() {
+		top = -Infinity;
+		right = -Infinity;
+		bottom = Infinity;
+		left = Infinity;
+
+    $grid = $('<div class="grid" />').appendTo('#game').on(
+			'click',
+      '.node:not(.grasp) .edge',
+      function() {
+				var node = $(this).parent()[0].node;
+
+				if ($(this).hasClass('active')) {
+					self.model.removeGrasp(node);
+					$(this).removeClass('active');
+				} else {
+					self.model.addGrasp(node);
+					$(this).addClass('active');
+				}
+			}
+		);
+
+    $('#nodes').on('click', '.remove', function() {
+      self.model.removeNode($(this).parent()[0].node);
+    }).on('change', '.x, .y', function() {
+      var t = parseFloat($(this).val());
+      var attr = $(this).hasClass('x') ? 'x' : 'y';
+      var $parent = $(this).parent();
+
+      if (isNaN(t)) {
+        t = $parent[0].node[attr];
+      }
+
+      $(this).val(t);
+
+      self.model.moveNode(
+				$parent[0].node,
+				$parent.children('.x').val(),
+				$parent.children('.y').val()
+			);
+    }).on('focus', '.x, .y', function() {
+      $(this).parent()[0].node.$node.addClass('active');
+    }).on('blur', '.x, .y', function() {
+      $(this).parent()[0].node.$node.removeClass('active')
+    });
+
+    $('#add').click(function() {
+      self.model.appendNode();
+      $('#nodes > :last-child .x').focus();
+    });
+
+    $('#mu').change(function() {
+      var t = parseFloat($(this).val());
+
+      if (isNaN(t)) {
+        t = self.model.getMu();
+      }
+
+      $(this).val(t);
+      self.model.setMu(t);
+    }).val(mu).change();
+  };
+}
